9876c1c2a89be02377c8c5aa94ae163e956a5a2f
dinhphu2k1-gif <dinhphu012001@gmail.com>
2023-06-07 23:14:32 +0700

    update

--- loader-kafka/src/main/java/org/hust/utils/KafkaUtils.java
+++ loader-kafka/src/main/java/org/hust/utils/KafkaUtils.java
@@ -4,17 +4,15 @@ import lombok.Getter;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.hust.config.ConfigInfo;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Getter
 public class KafkaUtils {
-    private Map<String, Object> kafkaParams = null;
-    Collection<String> topics = null;
+    private final Map<String, Object> kafkaParams = new HashMap<>();
+    private final Collection<String> topics;
 
     public KafkaUtils(String groupId, String... topics) {
+
         kafkaParams.put("bootstrap.servers", ConfigInfo.Kafka.KAFKA_HOST);
         kafkaParams.put("key.deserializer", StringDeserializer.class);
         kafkaParams.put("value.deserializer", StringDeserializer.class);
