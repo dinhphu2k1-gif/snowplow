32e2c8791ed9a7698b7b5a4077192af55a5663ab
dinhphu2k1-gif <dinhphu012001@gmail.com>
2023-07-26 02:44:19 +0700

    check time stream

--- loader-kafka/bin/run_stream.sh
+++ loader-kafka/bin/run_stream.sh
@@ -2,7 +2,7 @@
 --num-executors 2 --executor-memory 1g --executor-cores 2 \
 --keytab /home/phuld/phuld_platform.keytab --principal phuld/platform@HADOOP.SECURE \
 --queue datamining \
---files=properties/GeoLite2-City.mmdb \
+--files=./properties/GeoLite2-City.mmdb \
 --conf spark.streaming.kafka.allowNonConsecutiveOffsets=true \
 --class org.hust.job.JobsManager target/loader-kafka-1.0-SNAPSHOT-jar-with-dependencies.jar  \
 --job CollectEvent --duration $1 --groupId $2 --topics $3
