35b2624b2579a33e6124962f143c2d276844c0c0
phuld <phuledinh@channelvn.net>
2023-07-26 15:06:14 +0700

    update

--- loader-kafka/bin/run_report.sh
+++ loader-kafka/bin/run_report.sh
@@ -1,1 +1,6 @@
-/opt/spark320/bin/spark-submit --master yarn  --deploy-mode client --keytab /home/phuld/phuld_platform.keytab --principal phuld/platform@HADOOP.SECURE --queue datamining --conf spark.streaming.kafka.allowNonConsecutiveOffsets=true --class org.hust.job.report.AggregateData target/loader-kafka-1.0-SNAPSHOT-jar-with-dependencies.jar
+/opt/spark320/bin/spark-submit \
+--master yarn  --deploy-mode client \
+--num-executors 2 --executor-memory 1g --executor-cores 2 \
+--keytab /home/phuld/phuld_platform.keytab --principal phuld/platform@HADOOP.SECURE \
+--queue datamining --conf spark.streaming.kafka.allowNonConsecutiveOffsets=true \
+--class org.hust.job.report.AggregateData target/loader-kafka-1.0-SNAPSHOT-jar-with-dependencies.jar
